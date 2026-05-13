a07fde9e1e673b3898291520467ff408327b040f
daniel_shr <daniel_schr@tu-berlin.de>
2026-05-13 12:21:57 +0200

    sorted init and marked functions and classes to represent Tutorials
    added docstrings to all public functions

--- src/pydpeet/io/read.py
+++ src/pydpeet/io/read.py
@@ -23,6 +23,29 @@ def read(
     keep_all_additional_data: bool = False,
     custom_folder_path: Optional[str] = None,
 ) -> pd.DataFrame | list[pd.DataFrame]:
+    """
+    Reads a measurement file or directory of files and returns the standardized DataFrame or list of DataFrames.
+    The config determines the reader, formatter and mapper to use.
+
+    Parameters
+    ----------
+    config : ReadConfig or str
+        The configuration to use for standardizing the file or directory.
+    input_path : object
+        The path to the file or directory to standardize.
+    keep_all_additional_data : bool, optional
+        Whether to keep all additional data in the output DataFrame. If False, any
+        columns not specified in the configuration will be dropped. Defaults to
+        False.
+    custom_folder_path : str, optional
+        The path to the directory containing the custom reader, mapper, and
+        formatter reference for the given configuration.
+
+    Returns
+    -------
+    pd.DataFrame or list[pd.DataFrame]
+        The standardized DataFrame or list of DataFrames.
+    """
     # Validate boolean parameter using guardrail
     _guardrail_boolean(keep_all_additional_data, hard_fail_none=True, hard_fail_wrong_type=True)
 
